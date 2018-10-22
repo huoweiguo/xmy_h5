d96e5d4999b10cb498b6407f85fa5232f42029ef
huoweiguo <592871458@qq.com>
2018-10-22 17:16:28 +0800

    midify for home page

--- src/module/home/App.vue
+++ src/module/home/App.vue
@@ -3,30 +3,35 @@
         <div class="body-mask"></div>
         
         <div class="home-list">
+            <div class="home-tj">推荐</div>
+
             <ul v-show="recruitList.length > 0">
-                <li v-for="item in recruitList" @click="gozp(item.recruitId)" v-if="item.type == 'Recruit'">
-                    <div class="list-block1">
-                        <div class="block-box1">
-                            <b>{{item.position}}</b><i>招{{item.peoples}}名</i>
-                            <div class="block-list">
-                                <span>{{item.workCity}}</span>
-                                <span>{{item.workDistrict}}</span>
-                                <span>{{item.minAge}}~{{item.maxAge}}岁</span>
-                                <span>{{item.educationDesc}}</span>
+                <li v-for="item in recruitList" @click="gozp(item.recruitId,item.userId,item.educationDesc,item.yunxinId)" v-if="item.type == 'Recruit'">
+                    <div class="li-content">
+                        <div class="block-headImg"><img :src="item.userAvatar"></div>
+                        <div class="list-block1">
+                            <div class="block-box1">
+                                <b>{{item.position}}({{item.peoples}}名)</b>
+                                <div class="block-list">
+                                    <span>{{item.workCity}}</span>
+                                    <span>{{item.workDistrict}}</span>
+                                    <span>{{item.minAge}}~{{item.maxAge}}岁</span>
+                                    <span>{{item.educationDesc}}</span>
+                                </div>
                             </div>
+                            <span class="block-span">{{item.minWages}}-{{item.maxWages}}{{item.wagesUnitDesc}}/{{item.wagesComputationTimeDesc}}</span>
                         </div>
-                        <span class="block-span">{{item.minWages}}-{{item.maxWages}}{{item.wagesUnitDesc}}/{{item.wagesComputationTimeDesc}}</span>
-                    </div>
-                    <div class="list-block2">
-                        <div class="user-msg"><img :src="item.userAvatar">{{item.employerDesc}}</div>
-                        <div class="user-level">
-                            <span>要求：</span> 
-                            <div class="level-star">
-                                <div v-if="item.starLevel ==1" class="lev1"></div>
-                                <div v-if="item.starLevel ==2" class="lev2"></div>
-                                <div v-if="item.starLevel ==3" class="lev3"></div>
-                                <div v-if="item.starLevel ==4" class="lev4"></div>
-                                <div v-if="item.starLevel ==5" class="lev5"></div>
+                        <div class="list-block2">
+                            <div class="user-msg">{{item.employerDesc}}</div>
+                            <div class="user-level">
+                                <span>要求&nbsp;</span> 
+                                <div class="level-star">
+                                    <div v-if="item.starLevel ==1" class="lev1"></div>
+                                    <div v-if="item.starLevel ==2" class="lev2"></div>
+                                    <div v-if="item.starLevel ==3" class="lev3"></div>
+                                    <div v-if="item.starLevel ==4" class="lev4"></div>
+                                    <div v-if="item.starLevel ==5" class="lev5"></div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -36,14 +41,14 @@
         
         <div class="online">
             <ul>
-                <li v-for="item in recruitList" :data-id="item.productId" :data-uid="item.productUserId" v-if="item.type == 'Loan'" @click="golink(item.productId, item.productUserId, item.productName)">
+                <li v-for="item in recruitList" :data-id="item.productId"  :data-uid="item.productUserId" v-if="item.type == 'Loan'" @click="golink(item.productId, item.productUserId, item.productName)">
                     <img :src="item.bannerUrl"/>
                 </li>
             </ul>
         </div>
 
-        <div class="repayment-ts">
-            <span>待还金额: <b>1004.45元</b></span>
+        <div class="repayment-ts" v-show="loan">
+            <span>待还金额: <b>{{charge}}元</b></span>
             <a href="javascript:;" @click="goOrder">立即还款</a>
         </div>
     </div>
@@ -59,7 +64,9 @@
                 userId: xmy.getQueryString('userId'),
                 appVersion: xmy.getQueryString('appVersion'),
                 device: xmy.getQueryString('device'),
-                channelType: xmy.getQueryString('channelType')
+                channelType: xmy.getQueryString('channelType'),
+                loan: false,
+                charge: ''
             }   
         },
 
@@ -89,8 +96,8 @@
             },
 
 
-            gozp (recruitId) {
-                window.location.href = '/api/static/app_xmy/zpdetail?recruitId='+recruitId;
+            gozp (recruitId,uid,educationDesc,yunxinId) {
+                window.location.href = '/api/static/app_xmy/zpdetail?recruitId='+recruitId+'&educationDesc='+educationDesc+'&userId='+uid+'&yunxinId='+yunxinId;
             },
 
             golink (productId,productUserId,productName) {
@@ -120,11 +127,33 @@
 
             goOrder () {
                 window.location.href = '/api/static/xmy/module/order.html?token='+  this.token + '&userId='+this.userId;
+            },
+
+            render_loan () {
+                var _this = this;
+                $.ajax({
+                    url: '/gateway/api/order/billOrder/queryOverduePopTip',
+                    type: 'POST',
+                    data: {
+                        userId : _this.userId,
+                        token: _this.token,
+                        userType: 'B'
+                    },
+                    success: function(res){
+                        if(res.respCode == '000000'){
+                            if(res.data.isPopRepayTip == 'Y' || res.data.isPopWithdrawTip == 'Y'){
+                                _this.charge = res.data.repaymentAmt;
+                                _this.loan = true;
+                            }
+                        }
+                    }
+                });
             }
         },
 
         mounted() {
             this.recruit();
+            this.render_loan();
         }
     }
 </script>
