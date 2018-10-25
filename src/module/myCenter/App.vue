74b4082dbd9471c4a3f3dc8c4447732e49e59312
崔淑雅 <cuishuya@shilupan.com>
2018-10-25 15:11:41 +0800

    个人中心接口获取

--- src/module/myCenter/App.vue
+++ src/module/myCenter/App.vue
@@ -8,7 +8,39 @@
                     <em>{{accountBalance}}</em>
                 </a>
             </li>
-            <li>
+            <li v-for="(item,index) in menuList" :key="index" v-if="item.id!=1">
+                <a v-if="item.id == 3" :href="order">
+                    <span>{{item.name}}</span>
+                    <img src="../../../static/images/rightarrow_1f.png"/>
+                    <em style="color:#FF7F26;">{{repayment}}</em>
+                </a>
+                <a v-if="item.id == 2" href="/api/static/xmy_app/stars">
+                    <span>{{item.name}}</span>
+                    <img src="../../../static/images/rightarrow_1f.png"/>
+                </a>
+                <a v-if="item.id == 4" :href="myCard">
+                    <span>{{item.name}}</span>
+                    <img src="../../../static/images/rightarrow_1f.png"/>
+                </a>
+                <a v-if="item.id == 5" href="/api/static/xmy_app/khfk">
+                    <span>{{item.name}}</span>
+                    <img src="../../../static/images/rightarrow_1f.png"/>
+                </a>
+                <a v-if="item.id == 6" :href="shareLink">
+                    <span>{{item.name}}</span>
+                    <img src="../../../static/images/rightarrow_1f.png"/>
+                </a>
+                <a v-if="item.id == 7" :href="creditCard">
+                    <span>{{item.name}}</span>
+                    <img src="../../../static/images/rightarrow_1f.png"/>
+                </a>
+                <a v-if="item.id == 8" href="/api/static/xmy_app/phone">
+                    <span>{{item.name}}</span>
+                    <img src="../../../static/images/rightarrow_1f.png"/>
+                </a>
+                
+            </li>
+            <!-- <li>
                 <a :href="order">
                     <span>我的订单</span>
                     <img src="../../../static/images/rightarrow_1f.png"/>
@@ -26,15 +58,15 @@
                     <span>我的银行卡</span>
                     <img src="../../../static/images/rightarrow_1f.png"/>
                 </a>
-            </li>
+            </li> -->
         <!-- </ul>
         <ol> -->
-            <li>
+            <!-- <li>
                 <a :href="shareLink">
                     <span>邀请返现金</span>
                     <img src="../../../static/images/rightarrow_1f.png"/>
                 </a>
-            </li>
+            </li> -->
             <!-- <li>
                 <a :href="creditCard">
                     <span>信用卡取现</span>
@@ -49,24 +81,24 @@
             </li> -->
         <!-- </ol>
         <ul> -->
-            <li>
+            <!-- <li>
                 <a href="/api/static/xmy_app/khfk">
                     <span>客服与反馈</span>
                     <img src="../../../static/images/rightarrow_1f.png"/>
                 </a>
-            </li>
+            </li> -->
             <li>
                 <a href="/api/static/xmy_app/setting">
                     <span>设置</span>
                     <img src="../../../static/images/rightarrow_1f.png"/>
                 </a>
             </li>
-            <li>
+            <!-- <li>
                 <a href="/api/static/xmy_app/phone">
                     <span>手机检测</span>
                     <img src="../../../static/images/rightarrow_1f.png"/>
                 </a>
-            </li>
+            </li> -->
         </ul>
         <!-- 原生弹窗 -->
         <div class="popup" v-show="toLeave">
@@ -88,13 +120,18 @@ export default {
             accountBalance:'',
             token: xmy.getQueryString('token'),
             userId: xmy.getQueryString('userId'),
+            appVersion: xmy.getQueryString('appVersion'),
+            device: xmy.getQueryString('device'),
+            channelType: xmy.getQueryString('channelType'),
+            appId: 'xiaomuyu',
             repayment:'',
             shareLink:'',
             myCard:"/api/static/xmy/module/bankCard.html?userId="+xmy.getQueryString('userId')+"&token="+xmy.getQueryString('token'),
             balance:'',
             order:'',
             toLeave: false,
-            bankCardAuth:''
+            bankCardAuth:'',
+            menuList:[]
         }
     },
     methods: {
@@ -116,10 +153,32 @@ export default {
             let _this = this;
             _this.toLeave = false;
             window.location.href = '/api/static/xmy_app/stars'
+        },
+        getMenu () {
+            let _this = this;
+            $.ajax({
+            url: '/gateway/api/proxy/jbj/personalList?t='+(new Date()).getTime(),
+            type: 'post',
+            data: {
+                token: _this.token,
+                appId: _this.appId,
+                appVersion: _this.appVersion,
+                channelType: _this.channelType,
+                device: _this.device
+            },
+            success: function(res){
+                if(res.respCode == "000000"){
+                    _this.menuList = res.voList;
+                }else{
+                    xmy.toast(res.respMsg)
+                }
+            }
+        })
         }
     },
     mounted () {
         let _this = this;
+        _this.getMenu ();
         $.ajax({
             url: '/gateway/api/proxy/jbj/getUserDetail?t='+(new Date()).getTime(),
             type: 'post',
@@ -141,6 +200,8 @@ export default {
                     _this.bankCardAuth = res.userAuthInfo.bankCardAuth;
                     _this.balance = "/api/static/xmy/module/balance.html?userId="+_this.userId+"&token="+_this.token;
                     _this.order = "/api/static/xmy/module/order.html?userId="+_this.userId+"&token="+_this.token+"&linkType=mycenter";
+                }else{
+                    xmy.toast(res.respMsg)
                 }
             }
         })
