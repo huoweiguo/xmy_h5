30056ddfd23d100161d394bdf424ceabf2e55060
崔淑雅 <cuishuya@shilupan.com>
2018-10-26 13:25:35 +0800

    余额页面返回按钮

--- src/module/balance/App.vue
+++ src/module/balance/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
         <navigation>
-            <a href="javascript:;" slot="navigation_goback" class="navigation_goback"></a> 
+            <a href="javascript:;" @click="goCenter" slot="navigation_goback" class="navigation_goback"></a> 
             <span slot="navigation_title" class="navigation_title">余额</span>
             <a href="javascript:;" @click="goDetail" slot="navigation_small" class="navigation_small">余额明细</a>
         </navigation>
